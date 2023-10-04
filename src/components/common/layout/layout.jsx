75e8a50135ca34d2caf9d8c13300952d909c05b9
seongjin009 <iseongjin009@gmail.com>
2023-10-04 15:26:46 +0900

    글자 변경 후

--- src/components/common/layout/layout.jsx
+++ src/components/common/layout/layout.jsx
@@ -3,47 +3,31 @@ import clsx from 'clsx';
 import { useEffect, useRef, useState } from 'react';
 
 export default function Layout({ title, children, styleName }) {
-	//컴포넌트 마운트시 변경할 state추가
 	const [IsOn, setIsOn] = useState(false);
-	//가상돔 요소를 document.querySelector를 쓰지않고 실시간으로 참조하고 싶을떄
-	//빈 참조객체를 useRef로 생성
 	const frame = useRef(null);
 	const tit = useRef(null);
 
-	const splitText = (ref, gap, delay = 0) => {
-		let tags = '';
+	const splitText = (ref, gap = 0.1, delay = 0) => {
 		let count = 0;
-
-		//for of로 반복처리 하면서 span태그로 감싸줌
+		let tags = '';
 		for (let letter of ref.current.innerText) {
-			tags += `<span 'transitsiosn-delay:${gap * count + delay}s>${letter}</span>`;
+			tags += `<span style='transition-delay:${gap * count + delay}s'>${letter}</span>`;
 			count++;
 		}
-		//기존 참조객체 안쪽의 글자는 지워주고
 		ref.current.innerText = '';
-		//span으로 감싸준 문자열을 참조객체의 innerHTML으로 삽입
 		ref.current.innerHTML = tags;
 	};
 
 	useEffect(() => {
-		console.log('page 변경');
-		//컴포넌트 마운트시 한번만 호출
-		//컴포넌트 마운트시 IsOn값을 true로 변경
-		//IsOn을 useRef가 아닌 state로 변경해야 되는 이유
-		//useRef값을 변경해도 리액트는 변경점을 인지 못해서 재렌더링이 안되기 때문에
-		setIsOn(true);
-
+		//컴포넌트가 마운트되자마자 h1요소가 담겨있는 참조객체를 인수로 전달해서 글자 분리
 		splitText(tit, 0.1, 1);
+		//splitText함수가 기존 가상돔요소를 변경시 약간의 시간이 걸리기 때문에
+		//0.3초 뒤에 span으로 분리가 완료된 이후 on클래스를 추가
 		setTimeout(() => setIsOn(true), 300);
 	}, []);
 
 	return (
-		//참조하고싶은 가상돔 요소에 ref로 연결
-		<section
-			ref={frame}
-			//IsOn State가 true 일때에만 on 클래스명 적용
-			className={clsx(styles.layout, styleName, IsOn ? styles.on : '')}
-		>
+		<section ref={frame} className={clsx(styles.layout, styleName, IsOn ? styles.on : '')}>
 			<figure></figure>
 
 			<div className={clsx(styles.content, styleName)}>
